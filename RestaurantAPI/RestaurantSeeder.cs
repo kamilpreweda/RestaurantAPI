c7378655760e5f9d949ea6877b0a8f3641b81b1b
Kamil Preweda <preweda.kamil@o2.pl>
2022-08-07 11:27:59 +0200

    Created Restaurant Seeder

---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,87 @@
+﻿using RestaurantAPI.Entities;
+
+namespace RestaurantAPI
+{
+    public class RestaurantSeeder
+    {
+        private readonly RestaurantDbContext _dbContext;
+        public RestaurantSeeder(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    _dbContext.Restaurants.AddRange(restaurants);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            var restaurants = new List<Restaurant>();
+            {
+                new Restaurant()
+                {
+                    Name = "KFC",
+                    Category = "Fast Food",
+                    Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                    ContactEmail = "contact@kfc.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Nashville Hot Chicken",
+                            Price = 10.30M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Chicken Nuggehts",
+                            Price = 5.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Długa 5",
+                        PostalCode = "30-001"
+                    }
+                };
+                new Restaurant()
+                {
+                    Name = "McDonald's",
+                    Category = "Fast Food",
+                    Description = "McDonald's Corporation is an American-based multinational fast food chain, founded in 1940 as a restaurant operated by Richard and Maurice McDonald",
+                    ContactEmail = "contact@mcdonalds.com",
+                    HasDelivery = true,
+                    Dishes = new List<Dish>()
+                    {
+                        new Dish()
+                        {
+                            Name = "Big Mac",
+                            Price = 5.50M,
+                        },
+                        new Dish()
+                        {
+                            Name = "Mc Chicken",
+                            Price = 3.30M,
+                        },
+                    },
+                    Address = new Address()
+                    {
+                        City = "Kraków",
+                        Street = "Szewska 2",
+                        PostalCode = "31-009",
+                    }
+                };
+                return restaurants;
+            }
+        }
+    }
+}
